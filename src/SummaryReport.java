f95a197da9f5cb3953dacda6efb93bb782019ee9
TJ Trueblood <tbtrueblood@crimson.ua.edu>
2021-11-30 06:51:48 +0000

    added instance variables and framework for new methods

--- src/SummaryReport.java
+++ src/SummaryReport.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /*
 Group Member: TJ Trueblood
 Class: SummaryReport.java
@@ -8,21 +14,38 @@ Status: Incomplete
 public class SummaryReport {
 
     // private variables to hold summary report data
-    private String summaryReportProviderList;
-    private int summaryReportConsultationsNum;
-    private double summaryReportTotalFee;
-    private int summaryReportProviderTotal;
-    private int summaryReportTotalConsultations;
-    private double summaryReportOverallFee;
+    private ArrayList<Provider> providerList = new ArrayList<Provider>();
+    private int totalNumberProviders;
+    private int totalNumberConsultations;
+    private float totalFeesOwed;
+    private int totalProviderFees[];
 
+    
+    
     // initialize the variables to null
     public void summaryReport() {
-        summaryReportProviderList = null;
-        summaryReportConsultationsNum = 0;
-        summaryReportTotalFee = 0;
-        summaryReportProviderTotal = 0;
-        summaryReportTotalConsultations = 0;
-        summaryReportOverallFee = 0;
+        providerList = null;
+        totalNumberProviders = 0;
+        totalNumberConsultations = 0;
+        totalFeesOwed = 0;
+        totalProviderFees = null;
     }
 
+    public void writeToFile() throws IOException {
+    	String filename = ".\\src" + File.separator + "reports" + File.separator + "summary_report.txt";
+    	String filestring = "";
+    	filestring = filestring.concat
+    }
+    
+    public int sumGrandTotal() {
+    	
+    }
+    
+    public void getReports(int number) {
+    	
+    }
+    
+    public void getProviderSumFees(int number) {
+    	
+    }
 }
